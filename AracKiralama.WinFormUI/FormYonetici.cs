2a2c3af737aa12dcbc59235e02c953dcd4b5582e
mceylan35 <mehmetceylan2696@gmail.com>
2019-05-02 23:32:53 +0300

    form ui tasarım
    form

---
+++ AracKiralama.WinFormUI/FormYonetici.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AracKiralama.WinFormUI
+{
+    public partial class FormYonetici : Form
+    {
+        public FormYonetici()
+        {
+            InitializeComponent();
+        }
+
+        private void FormYonetici_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AracKiralama.WinFormUI/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AracKiralama.WinFormUI.AracServis;
+using AracKiralama.WinFormUI.KullaniciSoapServis;
+using AutoMapper;
+
+namespace AracKiralama.WinFormUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            mainPanel.Controls.Clear();
+            LoginForm loginForm = new LoginForm();
+            loginForm.TopLevel = false;
+            mainPanel.Controls.Add(loginForm);
+            loginForm.Show();
+            loginForm.Dock = DockStyle.Fill;
+        }
+    }
+}
